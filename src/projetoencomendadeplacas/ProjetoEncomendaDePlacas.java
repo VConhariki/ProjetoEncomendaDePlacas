44975a9f42fdd475dc7ada742111e8c2e925d4ea
Vagner de Oliveira Conhariki <vconhariki@gmail.com>
2023-11-15 21:11:45 -0300

    criado a primeira tela

--- src/projetoencomendadeplacas/ProjetoEncomendaDePlacas.java
+++ src/projetoencomendadeplacas/ProjetoEncomendaDePlacas.java
@@ -1,40 +1,23 @@
 package projetoencomendadeplacas;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ProjetoEncomendaDePlacas extends Application {
-    //teste
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(ProjetoEncomendaDePlacas.class.getResource("Screens/mainScreen.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Encomenda De Placas");
+        stage.setScene(scene);
+        stage. setResizable(false);
+        stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
