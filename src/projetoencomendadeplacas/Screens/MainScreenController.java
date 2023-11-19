0217e7ae4a5bf6d5d272bd00a4d8968c7a049620
Vagner de Oliveira Conhariki <vconhariki@gmail.com>
2023-11-19 14:34:43 -0300

    abrir novas janelas quando clicar nos panes da mainScreen

--- src/projetoencomendadeplacas/Screens/MainScreenController.java
+++ src/projetoencomendadeplacas/Screens/MainScreenController.java
@@ -1,18 +1,23 @@
 package projetoencomendadeplacas.Screens;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainScreenController implements Initializable {
+    
+    final Logger logger;
 
-    @FXML
-    private Pane clientePane;
-    @FXML
-    private Pane encomendaPane;
+    public MainScreenController() {
+        this.logger = Logger.getLogger(getClass().getName());
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -20,11 +25,33 @@ public class MainScreenController implements Initializable {
     }    
 
     @FXML
-    private void openClienteScreen(MouseEvent event) {
+    private void openClienteScreen() {
+        try {
+            setStage("clienteScreen.fxml", "Cliente Manager");
+            logger.info("Nova janela[Cliente] criada com sucesso");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Erro ao criar nova janela cliente.", e);
+        }
     }
 
     @FXML
-    private void openEncomendaScreen(MouseEvent event) {
+    private void openEncomendaScreen() {
+        try {
+            setStage("encomendaScreen.fxml", "Cliente Manager");
+            logger.info("Nova janela[Encomenda] criada com sucesso");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Erro ao criar nova janela Encomenda.", e);
+        }
     }
     
+    private void setStage(String path, String title) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource(path));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage. setResizable(false);
+        stage.show();
+    }
 }
